8edafc626eb8e8ef156db9a93daa5b69f95d5e84
Linus Willner <hello@linuswillner.me>
2020-02-29 18:14:50 +0200

    Always broadcast own encryption key on room join

--- src/ws/events/room_join.js
+++ src/ws/events/room_join.js
@@ -1,6 +1,9 @@
+const { encryptionKey } = require('../../crypto')
+
 module.exports = state => {
   const { room, socket } = state
   global.log.info(`Joined room ${room}.`)
   global.chatLog.info(`Joined room ${room}.`)
   socket.emit('chat_join', { id: socket.id })
+  socket.emit('broadcast_key', { id: socket.id, encryptionKey })
 }
