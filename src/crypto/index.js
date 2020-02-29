cafee62c7fc459b992a93481e80415dde2cf0ea3
Linus Willner <hello@linuswillner.me>
2020-02-29 18:15:53 +0200

    Fix AES-256-CBC buffer encoding issues and add anti-tampering

--- src/crypto/index.js
+++ src/crypto/index.js
@@ -13,7 +13,8 @@ exports.keyCache = new Map()
 exports.encryptMessage = message => {
   const cipher = crypto.createCipheriv('aes-256-cbc', key, iv)
   const encrypted = cipher.update(message)
-  return Buffer.concat([encrypted, cipher.final()])
+  const buffer = Buffer.concat([encrypted, cipher.final()])
+  return buffer.toString('hex') // Encode as hex string to prevent garbling
 }
 
 exports.decryptMessage = (message, decryptionKey) => {
@@ -24,5 +25,10 @@ exports.decryptMessage = (message, decryptionKey) => {
 
   const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv)
   const decrypted = decipher.update(text)
-  return Buffer.concat([decrypted, decipher.final()])
+  const buffer = Buffer.concat([decrypted, decipher.final()])
+  return buffer.toString() // Encode as normal UTF-8 string
 }
+
+// Freeze encryption key and seal key cache to prevent tampering
+Object.freeze(require.cache[module.id].exports.encryptionKey)
+Object.seal(require.cache[module.id].exports.keyCache)
