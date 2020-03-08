c032fbba4aeee139e96016a5e37a827448585631
Linus Willner <hello@linuswillner.me>
2020-03-08 20:43:45 +0200

    Highlight room name on join

--- src/ws/events/room_join.js
+++ src/ws/events/room_join.js
@@ -1,9 +1,9 @@
 const { encryptionKey } = require('../../crypto')
 
 module.exports = state => {
-  const { room, socket } = state
-  global.log.info(`Joined room ${room}.`)
-  global.chatLog.info(`Joined room ${room}.`)
+  const { socket, eventParams } = state
+  global.log.info(`Joined room ${eventParams.name} (ID ${eventParams.id}).`)
+  global.chatLog.info(`Joined room ${`${eventParams.name}`.bold.underline}.`)
   socket.emit('chat_join', { id: socket.id })
   socket.emit('broadcast_key', { id: socket.id, encryptionKey })
 }
